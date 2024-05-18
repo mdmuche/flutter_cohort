7a0289c82f2969697b0a9d7bf5699da57fab6fb4
mdmuche <abulamartins@gmail.com>
2024-05-18 20:07:30 +0100

    utils

---
+++ shopa/lib/utils/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xffF83758);
+const white = Colors.white;
+const black = Colors.black;
---
+++ shopa/lib/utils/textstyles.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:shopa/utils/colors.dart';
+
+TextStyle text36 = GoogleFonts.montserrat(
+  fontSize: 36,
+  fontWeight: FontWeight.w800,
+);
+TextStyle text24Bold = GoogleFonts.montserrat(
+  fontSize: 24,
+  fontWeight: FontWeight.w900,
+);
+
+TextStyle text20 = GoogleFonts.montserrat(
+  fontSize: 20,
+  fontWeight: FontWeight.w500,
+);
+TextStyle text18 = GoogleFonts.montserrat(
+  fontSize: 18,
+  fontWeight: FontWeight.w600,
+);
+TextStyle text16 = GoogleFonts.montserrat(
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+);
+
+TextStyle text14Light = GoogleFonts.montserrat(
+  fontSize: 14,
+  fontWeight: FontWeight.w300,
+  color: const Color(0xffA8A8A9),
+);
+TextStyle text14Bold = GoogleFonts.montserrat(
+  fontSize: 14,
+  fontWeight: FontWeight.w600,
+  color: primaryColor,
+);
+
+TextStyle text12 = GoogleFonts.montserrat(
+  fontSize: 12,
+  fontWeight: FontWeight.w300,
+  color: primaryColor,
+);
